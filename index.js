27c7fd74d3b74850794c6f1559af79f7280fced9
Alexander Brandstedt <abrandstedt@spotify.com>
2020-07-07 17:05:01 +0200

    adding prom-client

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const core = require('@actions/core');
+const client = require('prom-client');
 
 
 // most @actions toolkit packages have async methods
@@ -6,6 +7,8 @@ async function run() {
   try { 
     const pushgatewayAddr = core.getInput('pushgateway')
     console.log(`Got Prometheus Pushgateway address: ${pushgatewayAddr}`)
+
+    let gateway = new client.Pushgateway(pushgatewayAddr);
   }
   catch (error) {
     core.setFailed(error.message);
