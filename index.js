5e3f7a789172de8cb67606458c3325becb2a2e8b
Alexander Brandstedt <abrandstedt@spotify.com>
2020-07-07 16:49:16 +0200

    run test adding dist

--- index.js
+++ index.js
@@ -1,19 +1,12 @@
 const core = require('@actions/core');
-const wait = require('./wait');
 
 
 // most @actions toolkit packages have async methods
 async function run() {
   try { 
-    const ms = core.getInput('milliseconds');
-    console.log(`Waiting ${ms} milliseconds ...`)
-
-    core.debug((new Date()).toTimeString())
-    await wait(parseInt(ms));
-    core.debug((new Date()).toTimeString())
-
-    core.setOutput('time', new Date().toTimeString());
-  } 
+    const pushgatewayAddr = core.getInput('pushgateway')
+    console.log(`Got Prometheus Pushgateway address: ${pushgatewayAddr}`)
+  }
   catch (error) {
     core.setFailed(error.message);
   }
