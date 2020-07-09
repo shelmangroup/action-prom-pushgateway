f3a9ca6185b2ae55712041735af0e4c0dab95ed7
Alexander Brandstedt <alexander@infralium.com>
2020-07-09 15:07:53 +0200

    test

--- index.js
+++ index.js
@@ -2,6 +2,14 @@ const core = require('@actions/core');
 const github = require('@actions/github');
 const client = require('prom-client');
 
+function statusValue(input) {
+  var change_me = 0;
+  if (input === 'success') {
+    change_me = 1;
+  }
+  return change_me
+}
+
 async function run() {
   try { 
     const pushgatewayAddr = core.getInput('pushgateway')
@@ -9,47 +17,39 @@ async function run() {
     const Registry = client.Registry;
     const register = new Registry();
     const gateway = new client.Pushgateway(pushgatewayAddr, [], register);
-    const repo = github.context.repo.repo;
-    const owner = github.context.repo.owner;
-    // const run_id = github.context.runId;
+    const run_id = github.context.runId;
+    const job = github.context.job;
+    const repo = `github.context.repo: github.com/${github.context.repo.owner}/${github.context.repo.repo}`
 
     core.info(`Got Prometheus Pushgateway address: ${pushgatewayAddr}`)
-    core.info(`github.context.job: ${github.context.job}`)
-    core.info(`github.context.runId: ${github.context.runId}`)
-    core.info(`github.context.repo: github.com/${owner}/${repo}`)
+    core.info(`github.context.job: ${job}`)
+    core.info(`github.context.runId: ${run_id}`)
+    core.info(`github.context.repo: ${repo}`)
 
-    const test = new client.Counter({
-      name: `${prefix}_test`,
-      help: `${prefix}_test`,
+    const previousstate = core.getInput('previousstate');
+    core.info(`previousstate: ${previousstate}`);
+    const state_num = statusValue(previousstate)
+
+    const metric_action_gauge = new client.Gauge({
+      name: `${prefix}_run`,
+      help: `${prefix}_run`,
       registers: [register],
+      labelNames: ['repo', 'status', 'runid', `job`],
     });
-    register.registerMetric(test);
-    test.inc(10);
+    register.registerMetric(metric_action_gauge);
+    metric_action_gauge.set({
+      repo: repo,
+      status: previousstate,
+      run_id: run_id,
+      job: job,
+    }, state_num);
 
     await gateway.push({ jobName: prefix }, (err, resp, body) => {
       core.info(`Error: ${err}`);
       core.info(`Body: ${body}`);
     });
 
-    // octokit testing
-    // const token = core.getInput('token');
-    // const octokit = github.getOctokit(token);
-
-    // const jobsListWorkflowObj = await octokit.actions.listJobsForWorkflowRun({
-    //   owner,
-    //   repo,
-    //   run_id
-    // });
-
-    // const jobsList = JSON.stringify(jobsListWorkflowObj, undefined, 2)
-    // core.info(`jobsLit: ${jobsList}`)
-
-    const previousstate = core.getInput('previousstate');
-    core.info(`previousstate: ${previousstate}`);
-
-
-    core.info('mark end')
-
+    core.info('mark end');
 
   }
   catch (error) {
@@ -57,4 +57,4 @@ async function run() {
   }
 }
 
-run()
+run();
